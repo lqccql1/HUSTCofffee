3d02ab0193692697ad46f22ce96058b2ee77f763
lqccql1 <109381980+lqccql1@users.noreply.github.com>
2023-11-01 10:00:33 +0700

    Delete ALittleCoffeeShop directory

--- ALittleCoffeeShop/Library/src/main/java/com/ecommerce/library/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.library.repository;
-
-import com.ecommerce.library.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
